56c03442e4f83dfaf5b0f1df756a326b6e9c0d37
rcoliveira2016 <roramon7@gmail.com>
2024-05-26 22:48:47 -0300

    chat model

---
+++ LLMExemplosSimples/LLMExemplosSimplesEx1/LLMExemplosSimplesEx1.Chat/Program.cs
@@ -0,0 +1,45 @@
+﻿using LLama.Common;
+using LLama;
+
+string modelPath = @"./Phi-3-mini-4k-instruct-q4.gguf"; // change it to your own model path.
+
+var parameters = new ModelParams(modelPath)
+{
+    ContextSize = 1024, // The longest length of chat as memory.
+    GpuLayerCount = 5, // How many layers to offload to GPU. Please adjust it according to your GPU memory.
+};
+using var model = LLamaWeights.LoadFromFile(parameters);
+using var context = model.CreateContext(parameters);
+var executor = new InteractiveExecutor(context);
+
+// Add chat histories as prompt to tell AI how to act.
+var chatHistory = new ChatHistory();
+chatHistory.AddMessage(AuthorRole.System, "usar a lingua portugues brasileira.");
+
+ChatSession session = new(executor, chatHistory);
+
+InferenceParams inferenceParams = new InferenceParams()
+{
+    MaxTokens = 256, // No more than 256 tokens should appear in answer. Remove it if antiprompt is enough for control.
+    AntiPrompts = new List<string> { "User:" } // Stop generation once antiprompts appear.
+};
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("The chat session has started.\nUser: ");
+Console.ForegroundColor = ConsoleColor.Green;
+string userInput = Console.ReadLine() ?? "";
+
+while (userInput != "exit")
+{
+    await foreach ( // Generate the response streamingly.
+        var text
+        in session.ChatAsync(
+            new ChatHistory.Message(AuthorRole.User, userInput),
+            inferenceParams))
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(text);
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    userInput = Console.ReadLine() ?? "";
+}
